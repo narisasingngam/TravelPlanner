a43d2f2c331737da7cb4591f8a5358eacabf9c58
jampttws <tanasorn42@hotmail.com>
2018-10-21 13:39:11 +0700

    add more function

--- map/views.py
+++ map/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Location
+import urllib.request
+import json
+import ssl
+
+api_key = 'AIzaSyBENVTYtp6UnlTVs8gmLomS1NNlJqK7-ww'
 
 def index(request):
     if request.method == 'POST':
@@ -12,3 +17,33 @@ def index(request):
                 return render(request, 'index.html')  
     else:
         return render(request,'index.html')
+
+def search_place(search):
+    endpoint = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'
+    inputtype = 'textquery&fields=formatted_address,name,opening_hours&locationbias=circle:2000@47.6918452,-122.2226413'
+    context = ssl._create_unverified_context()
+    
+    text_request = endpoint + f'input={search}&inputtype={inputtype}&key={api_key}'
+
+    response = urllib.request.urlopen(text_request, context=context).read()
+    direction = json.loads(response.decode('utf-8'))
+    
+    place  = direction['candidates'][0]['formatted_address']
+    return HttpRequest(place)
+
+def  time_count(origin, destination, mode):
+    endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    context = ssl._create_unverified_context()
+
+    text_request = endpoint + f'origins={origin}&destinations={destination}&mode={mode}&key={api_key}'
+
+    response = urllib.request.urlopen(request, context=context).read()
+    direction = json.loads(response.decode('utf-8'))
+
+    time = direction['rows'][0]['elements'][0]['duration']['text'])
+    return HttpRequest(time)
+
+
+
+ 
+    
