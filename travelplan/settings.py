fbeb7f8a3ba2b8f10a6c21e975af25b36b99c8a8
narisasingngam <mmintttt@gmail.com>
2018-10-22 15:02:24 +0700

    edited test database

--- travelplan/settings.py
+++ travelplan/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import sys
 import os
 import dj_database_url
 
@@ -76,22 +77,23 @@ WSGI_APPLICATION = 'travelplan.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': '',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '',
-    #     'HOST': '',
-    #     'PORT': '',
-    # },
-    'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }       
-}
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': '',
+        }
+    }
 
-# db_url = 'postgres://qykycimabhjaji:52fb48a92b28a36cb3ab4f3003149d2a9f48d826e23dc7956c8bb3ebeac78313@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d1a5jdq3qb959'
-# DATABASES['default'] = dj_database_url.config(default=db_url, conn_max_age=600, ssl_require=True)
+#Heroku-Postgres database
+db_url = 'postgres://qykycimabhjaji:52fb48a92b28a36cb3ab4f3003149d2a9f48d826e23dc7956c8bb3ebeac78313@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d1a5jdq3qb959'
+DATABASES['default'] = dj_database_url.config(default=db_url, conn_max_age=600, ssl_require=True)
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'mint2840',
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
