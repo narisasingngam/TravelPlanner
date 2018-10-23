3a7815932763cff9e521bbed5281f713572853bb
narisasingngam <mmintttt@gmail.com>
2018-10-23 14:46:03 +0700

    changed model

--- map/models.py
+++ map/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-class Location(models.Model):    
+class Location(models.Model):  
+    # Model representing name location  
     location = models.CharField(max_length = 30)
     stay_time = models.IntegerField(null=True)
+    # User can have multi-plan
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f'{self.location}, {self.stay_time}'
+    def get_absolute_url(self):
+        return reverse('planner-detail', args=[str(self.id)])
+
+class User(models.Model):
+
+    email = models.CharField(max_length = 30)
+    #the time that user want to go in each place
+    time = models.IntegerField(null=True)
+    date = models.DateField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.email}, {self.date}'
