3e6f14de9efef0ee111546b641a5a1bc73f0a7ef
narisasingngam <mmintttt@gmail.com>
2018-10-31 01:03:32 +0700

    added auto-complete url

--- map/urls.py
+++ map/urls.py
@@ -5,5 +5,6 @@ from map import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_place, name='place'),
-    path('time/', views.time_count, name='time')
+    path('time/', views.time_count, name='time'),
+    path('auto-complete/',views.auto_complete,name='auto')
 ]
