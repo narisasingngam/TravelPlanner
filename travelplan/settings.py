ae3488e45830ec1fa2495d55e81e69217731d4d0
narisasingngam <mmintttt@gmail.com>
2018-12-01 14:31:32 +0700

    deploy

--- travelplan/settings.py
+++ travelplan/settings.py
@@ -43,12 +43,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL=False
+CORS_ORIGIN_ALLOW_ALL=True
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
     '127.0.0.1:8080',
     'https://travelplan-app.herokuapp.com/',
+    'https://travelplanner-app.herokuapp.com/'
 
 )
 
