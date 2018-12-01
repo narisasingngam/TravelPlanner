e2c06022e64fdeedb1b2419c0f340b87001ef04d
narisasingngam <mmintttt@gmail.com>
2018-12-01 15:25:51 +0700

    deploy

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://travelplanner-app.herokuapp.com"'
+  API_URL: '"http://localhost:8000"'
 })
